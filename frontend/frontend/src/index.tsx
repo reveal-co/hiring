cfc97eca234a9995bc1906279346f1fec92ebdad
yoggsoft <yoggsoft@gmail.com>
2021-12-02 00:21:20 +0100

    Install Mui

--- frontend/frontend/src/index.tsx
+++ frontend/frontend/src/index.tsx
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from '@mui/system';
+import theme from '../src/styles/theme';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
